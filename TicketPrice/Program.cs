d388f331a846a7df8cf47404e26f8b69b9a7c383
Tsvetelin Donkovski <tsvetelin.donkovski@gmail.com>
2023-09-26 21:28:04 +0300

    adding missed script
    Add missed script on previous commit

---
+++ TicketPrice/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Net.Sockets;
+
+namespace TicketPrice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+
+            if (type == "student")
+                Console.WriteLine("$1.00");
+            else if (type == "regular")
+                Console.WriteLine("$1.60");
+            else
+                Console.WriteLine("Invalid ticket type!");
+        }
+    }
+}
